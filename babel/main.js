ac67d1ddf0ca1c42f235974fe7c4de53ef87a288
Dmitry Farino <dfarino46@gmail.com>
2025-02-17 03:12:52 +0300

    ХУЙ

--- babel/main.js
+++ babel/main.js
@@ -2,10 +2,35 @@ const Ui = {
   headerInit: function () {
     document.querySelectorAll('.header').forEach(function (header) {
       const menuBtn = header.querySelector('.header__menu-btn');
+      const menu = document.querySelector('.menu');
 
       if (menuBtn) {
         menuBtn.addEventListener('click', function () {
           document.body.classList.toggle('--menu-opened');
+        });
+      }
+
+      if (menu) {
+        let listener = SwipeListener(menu);
+        
+
+        document.addEventListener('click', function (e) {
+          if (
+            !e.target.closest('.menu') &&
+            document.body.classList.contains('--menu-opened') &&
+            !e.target.closest('.header__menu-btn')
+          ) {
+            document.body.classList.remove('--menu-opened');
+          }
+        });
+
+        menu.addEventListener('swipe', function (e) {
+          if (
+            document.body.classList.contains('--menu-opened') &&
+            e.detail.directions.left
+          ) {
+            document.body.classList.remove('--menu-opened');
+          }
         });
       }
     });
